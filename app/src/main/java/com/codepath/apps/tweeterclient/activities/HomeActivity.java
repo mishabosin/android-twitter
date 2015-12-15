1bcab667a044cd7e6e864a585fa85f9f17ea67e3
Misha Bosin <misha@uber.com>
2015-12-14 17:52:43 -0800

    Added the profile menu icon

--- app/src/main/java/com/codepath/apps/tweeterclient/activities/HomeActivity.java
+++ app/src/main/java/com/codepath/apps/tweeterclient/activities/HomeActivity.java
@@ -1,15 +1,17 @@
 package com.codepath.apps.tweeterclient.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 import com.codepath.apps.tweeterclient.R;
 import com.codepath.apps.tweeterclient.adapters.TweetsPagerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +27,16 @@ public class HomeActivity extends AppCompatActivity {
 
         PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         tabStrip.setViewPager(vpPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        return true;
+    }
+
+    public void onProfileView(MenuItem menuItem) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
